eda9325a9dde0bc74193edb2f511b73d2ee62cc7
xtaide <xtaide@icloud.com>
2025-12-30 17:51:35 +0300

    fix: prettier config

--- src/prettier.config.mjs
+++ src/prettier.config.mjs
@@ -1,20 +1,20 @@
-/** @type {import("prettier").Config}  */
+/** @type {import("prettier").Config} */
 export default {
-  trailingComma: "none",
-  tabwidth: 4,
-  useTabs: true,
-  semi: false,
-  singleQuote: true,
-  jsxSingleQuote: true,
-  arrowParens: "avoid",
-  importOrderSeparation: true,
-  importOrderSortSpecifiers: true,
-  importOrderCaseInsensitive: true,
-  importOrderParserPlugins: [
-    "classProperties",
-    "decorators-legacy",
-    "typescript"
-  ],
-  importorder: ["<THIRD_PARTY_MODULES›", "^@/ (.*) $", "^../(.*)", "^. / (.*)"],
-  plugins: ["@trivago/prettier-plugin-sort-imports"],
-};
+	trailingComma: 'none',
+	tabWidth: 4,
+	useTabs: true,
+	semi: false,
+	singleQuote: true,
+	jsxSingleQuote: true,
+	arrowParens: 'avoid',
+	importOrderSeparation: true,
+	importOrderSortSpecifiers: true,
+	importOrderCaseInsensitive: true,
+	importOrderParserPlugins: [
+		'classProperties',
+		'decorators-legacy',
+		'typescript'
+	],
+	importOrder: ['<THIRD_PARTY_MODULES>', '^@/(.*)$', '^../(.*)', '^./(.*)'],
+	plugins: ['@trivago/prettier-plugin-sort-imports']
+}
